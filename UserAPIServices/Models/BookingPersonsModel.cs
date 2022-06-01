cd9a13d017d789f2845d3b36aa2bf41aebf84019
OohaKurapati <106581347+OohaKurapati@users.noreply.github.com>
2022-06-01 13:46:26 +0530

    Admin

---
+++ UserAPIServices/Models/BookingPersonsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserAPIServices.Models
+{
+    public class BookingPersonsModel
+    {
+        public Guid? Id { get; set; }
+        
+        public Guid? FlightBookingId { get; set; }//uniqueidentifier    
+        
+        public int? SeatNo { get; set; }
+        
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+        public string Gender { get; set; }
+       
+    }
+}
